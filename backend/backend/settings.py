36abaf1e01eb9d1eed5d3c9d3020d4236e6490c1
yana12-2021 <Yana12-2021@yandex.ru>
2026-06-10 00:16:18 +0300

    Add 26taski.ru to ALLOWED_HOSTS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '81.26.190.77', '26taski.ru']
 
 
 # Application definition
